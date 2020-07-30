a8f8b4f9a91c1d5f1b5e48a8df3d94e980b103f9
karthickkumar07 <32150948+karthickkumar07@users.noreply.github.com>
2020-07-30 20:42:56 +0530

    Add files via upload

---
+++ Day4/build/precache-manifest.71d765e74787e10bd1566bfc619b6b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5970add24fa6569291fba39e98a320f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9807f76a705daafb139",
+    "url": "/static/css/main.48edeba4.chunk.css"
+  },
+  {
+    "revision": "fe8508e76b7fdbfda8f4",
+    "url": "/static/js/2.6c6f839d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c6f839d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9807f76a705daafb139",
+    "url": "/static/js/main.63b9f0b3.chunk.js"
+  },
+  {
+    "revision": "8f4272b8893a15b8fcd0",
+    "url": "/static/js/runtime-main.5aa31f1b.js"
+  }
+]);
---
+++ Day4/src/Components/Projects.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "./Projects.css";
+import "../Portfolio.css";
+
+const Projects = () => {
+  return (
+    <div className="row">
+      <div className="card">
+        <img
+          src="https://lh3.googleusercontent.com/vJ0H5WPP5HVxkyMbItTnbcATn57uLX5WshgE4HxJlbk4OvZDPLuilwEtVDrciXXInCZG4QQz5lliLoy_1M23Iyvq6o8ur8Z4gA1eteBjJV7IolnkwbHk779OzCts-MwfIdMFQYIs5kFM7XuINI4Vyt4Rd6lxousKD6mqJwi2MqMPyvGqzZ38up3YvP_g1cTq8_1EQQrofDMsMN6CsMmctXYo7gswZRc5GbwaFrxGvpV705sIi23YKwA8msGA_AqnPuusr8BN2K04nBdPOZj_PHThqk4eSmFT6y0KgxFZ5SfqEcrmrRSB2LrNfzMz1V0i4gd7UspXFcCKJY5R3V1935gfn9Y8uLOvjmvdenIvmh2Eks0cO8--FtlnluZjB25cU37Vr2uce-DTojLtf6Kc50wPLgqpWNU38YGJ0hj18P-7iMiWocm8jKY2ezTIe_xQf1t86_3Y4HYjrgCo3m83WYxD-9mMIuBP6bcHLE-5mKUFBx7WLZ5AKmud3QI1uIvz8n-wxRA23FY_vFjDWDeZfOkRvu6f6PRbvV1LONdBUDVmc-0v9RMNsCxDtxwsd7GyWhERfIEYW9SaaZ7lCmPtxI9YsKmBYN-T3m6Almkl-tBx6qF-0wUHjzlKIKJSfeJaeApZI9W5DNLPzQfEnNyWaEoSGOyuj4ZtZ_52wLRy7lFtuSaSITBfKNFQu8eg=w328-h185-no?authuser=0"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        />
+        <div className="container">
+          <h4 style={{ color: "blue" }}>Project Title</h4>
+          <h4>
+            <b> Peer To Peer File Transfer</b>
+          </h4>
+          <br />
+          <h4 style={{ color: "blue" }}>Project Description</h4>
+          <p>
+            Direct communication and file transfer without the help of server
+            using the webrtc technology
+          </p>
+          <br />
+          <h4 style={{ color: "blue" }}>Technologies</h4>
+          <p> 1. webrtc</p>
+          <p> 2. Html,css,javascipt</p>
+          <p> 3. Nodejs</p>
+          <p>4.Handlebars,Express</p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://lh3.googleusercontent.com/81DuXB2O3_FEaK93nDNr2sM-9PsCn-xFT7UEDIDvdQ65MBrwH6C3w39CtmkEww0m1w1daj-hVp04RwGp6VShcNvuV5GVED7mnAw82YQMfQMW0UTDxRkvCA1j86kH0PBShHx7l8rbdAB_Efg24ALofIJcqu0Hy_D9ex25M9Ow45ruajyXGT1I1JrTT35GtaaUKOkk0z_6xsgMiwtCLVkt8m1T76QmGFlvBoz5rX5wkeBtt7YkoABiyhJklifo-8dUdr0-kA6efNb-YQpjFS_-8sRJP5TMsf4HdBCi46FmxbP7tyoAaGIijKh72PQLSTyaMd1xyAiuIwT_IOlg_djWd7soqJyZ5ifPGbk5Qa5huNBFQNtVukJ7Pgkq7XWlIlfl-fEX4Ox_C8CYfU-TA5-9s-yujatuFao6xlkxmP5DdqkyHOU2QpAWYJ0eJFVLJlpMIg-VUYv1PumiCXxnJZfSHVgIPtRBZQdxuKIaHkb3WyQh24xa4LBDk1DL59kujQ9SZ9Pu7ab5ZqxMEWg4IVK3G_WAsSsxhxExxy_vKYFBiVyEAddzSPmiAhGLRJW7uv43P6O-4E_c2908YNYPuXo5RhU_4T5ztdpJ7oBjFsKSPL4aVpPCtXU62ndtEd6KQ8i0_FYku9kgHvRvMliZQLc4amFazrK41Machs8XdhinOSuCzEfQ6okONv14wSVD=w641-h362-no?authuser=0"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        />
+        <div className="container">
+          <h4 style={{ color: "blue" }}>Project Title</h4>
+
+          <h4>
+            <b>E-commerce Website</b>
+          </h4>
+          <br />
+          <h4 style={{ color: "blue" }}>Project Description</h4>
+
+          <p>
+            Professional Online shopping Website with braintree payment system.{" "}
+          </p>
+          <br />
+          <h4 style={{ color: "blue" }}>Technologies</h4>
+          <p>1.MongoDb</p>
+          <p>2.Express</p>
+          <p>3.Reactjs</p>
+          <p>4.Nodejs</p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://lh3.googleusercontent.com/kfcYy8FU3_iayyzmEJt-NAs2eXA-se02ZDevfiZn6FHafS4AVe0I-sxelpE2aIidrp3Rw8AtFEHUUT-7pCCYc9SZAq98jGGS09XEZ9lChpzN70CLMdpVjtcJmyiJ199MQWnNB0JED1VBrQKxC5qQf6Su_vbGsuQSyc8Rn4UbxrusXCLgyO8PNNAUnekzd3EbyHAOPnOx4126LrQ9F3WmYxf77L7Kf7LHN4F7W4TFA_YXKugwWuxs1nyG-ppOdfuTcU8DmlpzK4dXmSlDUrgiU-tUBUXaiateIjcUCyK2GdiXwRCIsu8L3PJBlPlGwt3poa5R5bohf1L-z5Lo8xiaQxiin-hMjuTS6To2WqgBYgTwvLihhYzBwMhFajtY-LQnopfmQQ87XHhH5YKNpmTsd4OHNhu6_Z2jr3cFPYLBvkFyGbQ1eMPIv3bewxSh21sxIgAsjI2YUoBwC8cCgcHtDIpWMtSuyC7UpccZ5BKET5_QYh6DB_IHmVyR8qkhzqTfn8VZwO-5GYHwMRFf6ge2S3VxlsCYGLIAsy_3nRC2smBoQaIS98-MMs5gF9pr3EZPtfZ3S_nyu3r1NdsyXr_RYiiFmkTYrhiMrBNIJL3XPcqPIziIruIGv-zgVszG_OXkFYH2nKloMiSJAAOqs-S81LgCJOl2V9z5q3qeuuM9_-hHQcdg9yKfluPLuza2=w1203-h679-no?authuser=0"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        />
+        <div className="container">
+          <h4 style={{ color: "blue" }}>Project Title</h4>
+
+          <h4>
+            <b>Story Social App</b>
+          </h4>
+          <br />
+          <h4 style={{ color: "blue" }}>Project Description</h4>
+
+          <p>
+            Public social where user have to google signin to enter the page and
+            post his story and view public story as well as authors story
+          </p>
+          <br />
+          <h4 style={{ color: "blue" }}>Technologies</h4>
+          <p>1.MongoDb Atlas</p>
+          <p>2.Express</p>
+          <p>3.React</p>
+          <p>4.Nodejs</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
