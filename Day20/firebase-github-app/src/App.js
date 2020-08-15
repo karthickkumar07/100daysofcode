a25eec5b76ce0633d70c34739f3a70f34b0005a8
karthickkumar07 <32150948+karthickkumar07@users.noreply.github.com>
2020-08-15 23:02:12 +0530

    Add files via upload

---
+++ Day20/firebase-github-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import { UserContext } from "./context/UserContext";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import firebaseConfig from "./config/firebaseconfig";
+// init firebase
+firebase.initializeApp(firebaseConfig);
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
+  );
+};
+
+export default App;
---
+++ Day20/firebase-github-app/src/config/firebaseconfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyC27DkQ2CViCJWpzav-_IPfgWPu-uAtZxI",
+  authDomain: "mygitapp-5feb7.firebaseapp.com",
+  databaseURL: "https://mygitapp-5feb7.firebaseio.com",
+  projectId: "mygitapp-5feb7",
+  storageBucket: "mygitapp-5feb7.appspot.com",
+  messagingSenderId: "994114734054",
+  appId: "1:994114734054:web:98f1a66a48e4b1880c3827",
+  measurementId: "G-YMQP3MCKCW",
+};
